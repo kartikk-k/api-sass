6096df3d0231370f40fb014aab70ad0f5c97fb2e
Kartik Khorwal <hellokartikk@gmail.com>
2023-08-15 23:45:23 +0530

    add: client side logs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react'
 import Link from 'next/link'
-import { craeteCheckoutLink, createCustomerIfNull, isSubscribed } from '@/lib/stripe'
+import stripe, { craeteCheckoutLink, createCustomerIfNull, isSubscribed } from '@/lib/stripe'
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
@@ -10,16 +10,14 @@ async function Dashboard() {
 
     const session = await getServerSession(authOptions)
 
-    let api_key = null
+    // get user data
+    const user = await prisma.user.findFirst({
+        where: { email: session?.user?.email }
+    })
 
-    // checks if user is logged in
-    if (session) {
-        const user = await prisma.user.findFirst({
-            where: { email: session.user?.email }
-        })
+    // set api_key
+    const api_key = user?.api_key
 
-        api_key = user?.api_key
-    }
 
     // get stripe customer id
     const customer = await createCustomerIfNull()
@@ -31,6 +29,33 @@ async function Dashboard() {
     const checkoutLink = await craeteCheckoutLink(String(customer))
     console.log(checkoutLink)
 
+    // get recent logs
+    const logs = await prisma.log.findMany({
+        where: {
+            userId: user?.id
+        },
+        orderBy: {
+            created: 'desc'
+        },
+        take: 10
+    })
+
+    let current_usage = 0
+
+    // get current usage
+    if (isSub) {
+        const subscription = await stripe.subscriptions.list({
+            customer: String(user?.stripe_customer_id)
+        })
+
+        const invoice = await stripe.invoices.retrieveUpcoming({
+            subscription: String(subscription.data.at(0)?.id)
+        })
+
+        if (invoice) current_usage = invoice.amount_due / 100
+
+    }
+
     return (
         <div>
             {isSub ? (
@@ -39,8 +64,22 @@ async function Dashboard() {
                         <p>You are subscribed to API</p>
                     </div>
                     <div className='flex flex-col border rounded-lg'>
+                        <p className='font-semibold p-2 border-b' >Current usage</p>
+                        <p className='p-2 text-sm bg-zinc-100 text-zinc-700' >${current_usage}</p>
+                    </div>
+                    <div className='flex flex-col border rounded-lg'>
                         <p className='font-semibold p-2 border-b' >API Key</p>
                         <p className='p-2 text-sm bg-zinc-100 text-zinc-700' >{api_key}</p>
+                    </div>
+                    <div className='flex flex-col border rounded-lg'>
+                        <p className='font-semibold p-2 border-b' >Logs</p>
+                        {logs.map((log) => (
+                            <div key={log.id} className='flex gap-10 border-b items-center p-2 text-sm bg-zinc-100 text-zinc-700'>
+                                <code className='p-1 rounded-md bg-zinc-200'>{log.method}</code>
+                                <p>{log.status}</p>
+                                <p>{log.created.toLocaleString()}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             ) : (
